4e3ddacd5f506e88a1d1e961953b17548d8bbaec
Luca D'Amico <menteora@gmail.com>
2020-11-17 15:45:03 +0100

    fixed LogHelper

--- LogHelper.py
+++ LogHelper.py
@@ -3,18 +3,16 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from logging.handlers import RotatingFileHandler
 
-def create_timed_rotating_log_timebased(path, when="D"):
+def create_handler_file_timebased(path, when="D"):
 
     handler = TimedRotatingFileHandler(path,
                                        when=when,
                                        interval=1,
                                        backupCount=5)
-    logger.addHandler(handler)
-    return logger
+    return handler
     
-def create_timed_rotating_log_bytebased(path, byte=1000000):
+def create_handler_file_bytebased(path, byte=1000000):
 
     handler = RotatingFileHandler(path, maxBytes=byte,
                                   backupCount=5)
-    logger.addHandler(handler)
-    return logger
+    return  handler
