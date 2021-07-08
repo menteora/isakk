9be778669a5af3fa900f7a7eaf3e1e590cf65bb5
Luca D'Amico <menteora@gmail.com>
2021-07-08 09:14:02 +0200

    added mysqlhelper

---
+++ MssqlHelper.py
@@ -0,0 +1,15 @@
+# StandarClass
+from sqlalchemy import create_engine
+from pathlib import Path
+import json
+
+def getEngineFromConfig(config_file):
+    if Path(config_file).exists():
+        with open(config_file, 'r') as f:
+            config_json = json.load(f)
+    user = config_json['authentication']['user']
+    password = config_json['authentication']['password']
+    host = config_json['authentication']['host']
+    database = config_json['authentication']['database']
+    conn_str = 'mssql+pymssql://'+user+':'+password+'@'+host+'/'+database
+    return create_engine(conn_str)
