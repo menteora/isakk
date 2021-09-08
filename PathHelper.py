70069afda230d27c530cf0469880c16ecd998517
Luca D'Amico <menteora@gmail.com>
2021-09-08 16:51:36 +0200

    fix pathhelper and added file helper

--- PathHelper.py
+++ PathHelper.py
@@ -7,17 +7,32 @@ def getMainFolder():
     return os.path.dirname(os.getcwd())
 
 def getOutputPath(filename=None):
-    return os.path.join(getMainSubfolder('outputs'), filename)
+    folder_name = 'outputs'
+    if filename==None:
+        return getMainSubfolder(folder_name)
+    else:    
+        return os.path.join(getMainSubfolder(folder_name), filename)
 
 def getLogPath(filename=None):
-    return os.path.join(getMainSubfolder('logs'), filename)
+    folder_name = 'logs'
+    if filename==None:
+        return getMainSubfolder(folder_name)
+    else:    
+        return os.path.join(getMainSubfolder(folder_name), filename)
 
 def getInputPath(filename=None):
-    return os.path.join(getMainSubfolder('inputs'), filename)
+    folder_name = 'inputs'
+    if filename==None:
+        return getMainSubfolder(folder_name)
+    else:    
+        return os.path.join(getMainSubfolder(folder_name), filename)
 
 def getConfigPath(filename=None):
-    return os.path.join(getMainSubfolder('configs'), filename)
-
+    folder_name = 'configs'
+    if filename==None:
+        return getMainSubfolder(folder_name)
+    else:    
+        return os.path.join(getMainSubfolder(folder_name), filename)
 # def getAppDirectoryPath():
 #     return os.getcwd()
 
