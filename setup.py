f038daaa118d6c3ab297b3856bdfb1fddf71f9aa
Luca D'Amico <menteora@gmail.com>
2021-09-24 16:17:19 +0200

    removed requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='It Swiss Army Knife Kit - namecode: ISAKK',
     long_description=open('README.txt').read(),
-    install_requires=['numpy'],
+    # install_requires=['numpy'],
     url='https://github.com/menteora/isakk',
     author="Luca D'Amico",
     author_email='menteora@gmail.com'
