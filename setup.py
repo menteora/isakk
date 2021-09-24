7558e4e129eacccfa2c28613900d7e878eaf51d1
Luca D'Amico <menteora@gmail.com>
2021-09-24 16:13:21 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='isakk',
+    version='0.1',
+    packages=['isakk'],
+    license='MIT',
+    description='It Swiss Army Knife Kit - namecode: ISAKK',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/menteora/isakk',
+    author="Luca D'Amico",
+    author_email='menteora@gmail.com'
+)
