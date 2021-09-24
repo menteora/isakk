773f7ace1953c9cb34285512b1d51da7c47c71be
Luca D'Amico <menteora@gmail.com>
2021-09-24 16:42:56 +0200

    fixed requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='It Swiss Army Knife Kit - namecode: ISAKK',
     long_description=open('README.md').read(),
-    install_requires=['pandas'],
+    #install_requires=['pandas'],
     url='https://github.com/menteora/isakk',
     author="Luca D'Amico",
     author_email='menteora@gmail.com'
