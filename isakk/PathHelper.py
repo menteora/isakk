c227cf4f7fd8840a82f3e5dd5d1cc980be2bbe9a
Luca D'Amico <menteora@gmail.com>
2021-09-27 17:42:43 +0200

    added pathhelper with custom path

--- isakk/PathHelper.py
+++ isakk/PathHelper.py
@@ -1,38 +1,52 @@
 import os
 
-def getMainSubfolder(folder):
-    return os.path.join(getMainFolder(), folder)
+class PathHelper:
+    def __init__(self, path=None):
+        if path is None:
+            self.path = None
+        else:
+            self.path = path
 
-def getMainFolder():
-    return os.path.dirname(os.getcwd())
+    def setBasePath(path):
+        self.path = path
 
-def getOutputPath(filename=None):
-    folder_name = 'outputs'
-    if filename==None:
-        return getMainSubfolder(folder_name)
-    else:    
-        return os.path.join(getMainSubfolder(folder_name), filename)
+    def getMainSubfolder(self, folder):
+        return os.path.join(self.getMainFolder(), folder)
 
-def getLogPath(filename=None):
-    folder_name = 'logs'
-    if filename==None:
-        return getMainSubfolder(folder_name)
-    else:    
-        return os.path.join(getMainSubfolder(folder_name), filename)
+    def getMainFolder(self):
+        if self.path is not None:
+            base_path = self.path
+        else:
+            base_path = os.path.dirname(os.getcwd())
+        return base_path
 
-def getInputPath(filename=None):
-    folder_name = 'inputs'
-    if filename==None:
-        return getMainSubfolder(folder_name)
-    else:    
-        return os.path.join(getMainSubfolder(folder_name), filename)
+    def getOutputPath(self, filename=None):
+        folder_name = 'outputs'
+        if filename==None:
+            return self.getMainSubfolder(folder_name)
+        else:    
+            return os.path.join(self.getMainSubfolder(folder_name), filename)
 
-def getConfigPath(filename=None):
-    folder_name = 'configs'
-    if filename==None:
-        return getMainSubfolder(folder_name)
-    else:    
-        return os.path.join(getMainSubfolder(folder_name), filename)
+    def getLogPath(self, filename=None):
+        folder_name = 'logs'
+        if filename==None:
+            return self.getMainSubfolder(folder_name)
+        else:    
+            return os.path.join(self.getMainSubfolder(folder_name), filename)
+
+    def getInputPath(self, filename=None):
+        folder_name = 'inputs'
+        if filename==None:
+            return self.getMainSubfolder(folder_name)
+        else:    
+            return os.path.join(self.getMainSubfolder(folder_name), filename)
+
+    def getConfigPath(self, filename=None):
+        folder_name = 'configs'
+        if filename==None:
+            return self.getMainSubfolder(folder_name)
+        else:    
+            return os.path.join(self.getMainSubfolder(folder_name), filename)
 # def getAppDirectoryPath():
 #     return os.getcwd()
 
