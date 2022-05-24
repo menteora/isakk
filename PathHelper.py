616677688f9c98eddc628d5cf0fda5f33b445417
Luca D'Amico <menteora@gmail.com>
2022-05-24 14:55:32 +0200

    added setup

--- PathHelper.py
+++ PathHelper.py
@@ -7,18 +7,11 @@ def getMainFolder():
     return os.path.dirname(os.getcwd())
 
 def getOutputPath(filename=None):
-<<<<<<< HEAD
-    if filename == None:
-        return getMainSubfolder('outputs')
-    else:
-        return os.path.join(getMainSubfolder('outputs'), filename)
-=======
     folder_name = 'outputs'
     if filename==None:
         return getMainSubfolder(folder_name)
     else:    
         return os.path.join(getMainSubfolder(folder_name), filename)
->>>>>>> a615f2d858b9add8c9ade6d92cb57db29b882aec
 
 def getLogPath(filename=None):
     folder_name = 'logs'
@@ -28,18 +21,11 @@ def getLogPath(filename=None):
         return os.path.join(getMainSubfolder(folder_name), filename)
 
 def getInputPath(filename=None):
-<<<<<<< HEAD
-    if filename == None:
-        return getMainSubfolder('inputs')
-    else:
-        return os.path.join(getMainSubfolder('inputs'), filename)   
-=======
     folder_name = 'inputs'
     if filename==None:
         return getMainSubfolder(folder_name)
     else:    
         return os.path.join(getMainSubfolder(folder_name), filename)
->>>>>>> a615f2d858b9add8c9ade6d92cb57db29b882aec
 
 def getConfigPath(filename=None):
     folder_name = 'configs'
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="isakk",
+    version="0.0.1",
+    author="Luca D'Amico",
+    author_email="menteora@bancolini.com",
+    package_dir={
+        "isakk": ".",
+    },
+    packages=[
+        "isakk",
+    ],
+    description="It Swiss Army Knife Kit",
+    url="https://github.com/menteora/isakk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
