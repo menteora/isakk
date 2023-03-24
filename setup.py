16a83f14fefb1e2d6d9632b980bc0c0c66127d87
Luca D'Amico <menteora@gmail.com>
2023-03-24 16:32:05 +0100

    updated release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='isakk',
-    version='0.0.2',
+    version='0.0.3',
     packages=['isakk'],
     license='MIT',
     description='It Swiss Army Knife Kit - namecode: ISAKK',
