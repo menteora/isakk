c18a722ad1c516adc653816511b94b2e123c8c04
Luca D'Amico <menteora@gmail.com>
2023-11-13 15:30:35 +0100

    update relase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="isakk",
-    version="0.1.2",
+    version="0.1.3",
     author="Luca D'Amico",
     author_email="menteora@bancolini.com",
     package_dir={
